2cd34c7effca3e6a29c4e7939b488ea222c7bb9d
arioass <>
2021-11-20 08:46:51 -0300

    ajustes controller home

--- livrariaonline-api/src/main/java/br/com/alura/livrariaonlineapi/controller/HomeController.java
+++ livrariaonline-api/src/main/java/br/com/alura/livrariaonlineapi/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 
     @GetMapping
     public String home(){
-        return "Testando deploy auotmatizado";
+        return "Testando deploy automatizado";
     }
 }
